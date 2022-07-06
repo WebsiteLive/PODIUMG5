b31e8a98b4c4643ac6691233a9496ab6c137a6f3
Anna Fe Lachica <103997067+af-l@users.noreply.github.com>
2022-07-06 21:03:36 +0800

    DOA

---
+++ descriptionOfArt.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/descriptionOfArt.css">
+	<link rel="stylesheet" type="text/css" href="fontawesome/css/all.css">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700" rel="stylesheet">
+
+	
+
+	<title>Description of Art</title>
+	
+</head>
+
+<body>
+	<?php
+		include 'include/navbarnew.php';
+	?>
+	<div class="container">
+		<div class="col-sm-12"> Description of Art</div>
+	</div>	
+	
+
+	<div class="container">	  
+		<div class="row justify-content-around">
+		    <div class="col-sm-6">
+		    	<img class="img-fluid" src="sample.jpg" alt="artwork">
+		    </div>
+		    <div class="col-sm-6">
+
+				<div class ="container">
+					<div class="text">
+						<h4>The Virgin and Child with St Anne</h4>
+						<br>
+						<strong>Leonardo da Vinci</strong>
+						<br>
+						<h5>Oil on wood</h5>
+						<em>168 x 130 cm (5 1/2 x 4 1/2 ft.)</em>
+						<br><br><br>
+						<p>
+						This painting depicted St. Anne, her daughter the Virgin Mary, and the infant Jesus. Christ is shown grappling with a sacrificial lamb symbolizing his Passion whilst the Virgin tries to restrain him. The painting was commissioned as the high altarpiece for the Church of Santissima Annunziata in Florence and its theme had long preoccupied Leonardo.
+						<br>
+						<br>
+						Here, he has arranged the figures as a pyramid set in a landscape. While the theme of the Virgin Mary, her mother (Anne), and Jesus was common, it is unusual for Mary to be portrayed in her mother's lap. The background landscape, whose crags are seemingly replicated in Anne's veil, virtually melts in its sfumato haze. The baby lamb is both a symbol of innocence and of Jesus' sacrifice for humanity, memorialized in John the Baptist's reference to Jesus as the "Lamb of God".</p>
+					</div>
+						<div class="floating-parent">
+						<a href="http://localhost/PODIUM/FOR%20REVISION/INNAI/finalpodium.html" class="widgetlabel">Go to Artist's page  <i class="far fa-long-arrow-alt-right"></i></a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+		
+
+
+
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+	integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+	crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+	integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+	crossorigin="anonymous"></script>
+</body>
+</html>
+
