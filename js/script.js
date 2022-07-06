630732c3045fed7311afc0d137dce610e2341f57
Anna Fe Lachica <103997067+af-l@users.noreply.github.com>
2022-07-07 02:09:35 +0800

    active nav menu

--- js/script.js
+++ js/script.js
@@ -2,9 +2,14 @@ $(document).ready(function(){
     $(window).bind('scroll', function(){
         var gap = 50;
         if($(window).scrollTop()> gap){
-            $('header').addClass('active');
+            $('header').addClass('active-nav');
         } else{
-            $('header').removeClass('active');
+            $('header').removeClass('active-nav');
         }
     });
 });
+
+$('.nav-menu li a').click(function(){
+    $('.nav-menu li a').removeClass('active');
+    $(this).addClass('active');
+})
