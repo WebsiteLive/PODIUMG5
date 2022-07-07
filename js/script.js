46bceed9a07b4a7177be3d466a72779b906d8e57
Pigrolac <adrianpulao12@gmail.com>
2022-07-07 19:03:27 +0800

    Restore

--- js/script.js
+++ js/script.js
@@ -9,7 +9,14 @@ $(document).ready(function(){
     });
 });
 
+<<<<<<< Updated upstream
 $('.nav-menu li a').click(function(){
     $('.nav-menu li a').removeClass('active');
     $(this).addClass('active');
 })
+=======
+function togglePopup() {
+    document.getElementById("popup-1")
+     .classList.toggle("active");
+   }
+>>>>>>> Stashed changes
