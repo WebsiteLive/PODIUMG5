d818e15c05cf843b6c8d36f277168f42fa0a6de3
WebsiteLive <103171468+WebsiteLive@users.noreply.github.com>
2022-07-08 16:37:33 +0800

    freelancerpage adjustments

--- js/script.js
+++ js/script.js
@@ -1,27 +1,17 @@
 $(document).ready(function(){
 
-    $('#menu').click(function(){
+  $('#menu').click(function(){
       $(this).toggleClass('fa-times');
       $('header').toggleClass('toggle');
-    });
-  
-    $(window).on('scroll load',function(){
-  
-      $('#menu').removeClass('fa-times');
+  });
+
+  $(window).on('scroll load',function(){
+
+      $('#menu').removeClass('fa0-times');
       $('header').removeClass('toggle');
-  
-      if($(window).scrollTop() > 0){
-        $('.top').show();
-      }else{
-        $('.top').hide();
-      }
-  
-    });
-  
-    // smooth scrolling 
-  
-    $('a[href*="#"]').on('click',function(e){
-  
+  });
+  $('a[href*="#"]').on('click',function(e){
+
       e.preventDefault();
   
       $('html, body').animate({
@@ -35,4 +25,32 @@ $(document).ready(function(){
   
     });
   
+});
+
+function getImage(imagename){
+var newimg=imagename.replace(/^.*\\/,"");
+$('#display-image').html(newimg);
+}
+
+let preveiwContainer = document.querySelector('.products-preview');
+let previewBox = preveiwContainer.querySelectorAll('.preview');
+
+document.querySelectorAll('.products-container .product').forEach(product =>{
+product.onclick = () =>{
+  preveiwContainer.style.display = 'flex';
+  let name = product.getAttribute('data-name');
+  previewBox.forEach(preview =>{
+    let target = preview.getAttribute('data-target');
+    if(name == target){
+      preview.classList.add('active');
+    }
   });
+};
+});
+
+previewBox.forEach(close =>{
+close.querySelector('.fa-times').onclick = () =>{
+  close.classList.remove('active');
+  preveiwContainer.style.display = 'none';
+};
+});
