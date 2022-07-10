54825026d67075556736223ea0b5aa4d0d2ad60a
Anna Fe Lachica <103997067+af-l@users.noreply.github.com>
2022-07-10 10:51:51 +0800

    recolored logo

--- browse.php
+++ browse.php
@@ -30,7 +30,7 @@
 <body>
   <div class="sidebar">
     <div class="logo-details">
-      <img src="image/podium.png" alt="" class="icon">
+      <img src="img/wpodium.png" alt="" class="icon">
         <div class="logo_name"><a href="index.php" style="color: white;">&nbsp; &nbsp;PODIUM</a></div>
         <i class='bx bx-menu' id="btn" ></i>
     </div>
